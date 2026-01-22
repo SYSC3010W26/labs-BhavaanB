63116041a644292025553e73dfecb17292ba0be2
BhavaanB <145882411+BhavaanB@users.noreply.github.com>
2026-01-21 22:08:31 -0500

    Update lab2-database-data-visualizer.py

--- Lab2/lab2-database-data-visualizer.py
+++ Lab2/lab2-database-data-visualizer.py
@@ -2,16 +2,16 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Connect to database
+
 conn = sqlite3.connect("sensorDB.db")
 
-# Read table into pandas DataFrame
+
 df = pd.read_sql_query("SELECT * FROM sensordata", conn)
 
-# Convert datetime string to datetime object
+
 df["datetime"] = pd.to_datetime(df["datetime"])
 
-# Plot data
+
 plt.plot(df["datetime"], df["temperature"], label="Temperature")
 plt.plot(df["datetime"], df["humidity"], label="Humidity")
 plt.plot(df["datetime"], df["pressure"], label="Pressure")
